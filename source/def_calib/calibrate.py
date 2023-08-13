42f432a89228533e8ccd5a7b7fb38eb6d7711c68
sleekEagle <lahirunuwanisme@gmail.com>
2023-08-13 16:10:54 -0400

    calibrate camera with circular grid

--- source/def_calib/calibrate.py
+++ source/def_calib/calibrate.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import os
 
 def gaussian(x,sigma):
     g=np.exp(-1*(x)**2/(2*sigma**2))
@@ -28,6 +29,13 @@ def est_sigmas(img_path):
     grid_size=(4,11)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     ret, corners = cv2.findCirclesGrid(gray, grid_size,None,flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
+
+    cv2.imshow('img',gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
     if ret:
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         # img = cv2.drawChessboardCorners(gray.copy(), grid_size, corners2,ret)
@@ -94,5 +102,104 @@ def est_sigmas(img_path):
     else:
         return -1
 
-img_path=r'C:\Users\lahir\Downloads\circles.jpg'
-sigma_list,sigma_std_list=est_sigmas(img_path)
+photos_dir='C:\\Users\\lahir\\Downloads\\Photos-001\\'
+img_pths=os.listdir(photos_dir)
+img_pths.sort()
+
+sigma_list_list=[]
+fdist=[]
+for img in img_pths:
+    fdist.append(float(img[:-4]))
+    sigma_list,sigma_std_list=est_sigmas(os.path.join(photos_dir,img))
+    sigma_list_list.append(sigma_list)
+
+sig=[]
+for item in sigma_list_list:
+    sig.append(item[0])
+
+plt.plot(sig)
+plt.plot(fdist,sig)
+plt.show()
+
+kernel_size = 10
+kernel = np.ones(kernel_size) / kernel_size
+data_convolved = np.convolve(sig, kernel, mode='same')[int(kernel_size/2):]
+
+plt.plot(data_convolved)
+plt.show()
+
+
+def get_reprojection_errors(objpoints,imgpoints,rvecs,tvecs,mtx,dist):
+    errors=[]
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+        errors.append(error)
+    errors=np.array(errors)
+    print( "mean error: {}".format(np.mean(errors)))
+    return errors
+
+#calibrating with the asymetric circular grid 
+grid_size=(4,11)
+objp = np.zeros((grid_size[0]*grid_size[1], 3), np.float32)
+#distance between two circle centers in mm 
+d=42.5
+for i in range(grid_size[1]):
+    for j in range(grid_size[0]):
+        indx=grid_size[0]*i+j
+        x_value=i*d/2
+        y_value=j*d + d/2*(i%2)
+        objp[indx,:]=[x_value,y_value,0]
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+found=0
+photos_dir='C:\\Users\\lahir\\Downloads\\calib\\'
+img_pths=os.listdir(photos_dir)
+for item in img_pths:
+    gray=cv2.imread(os.path.join(photos_dir,item),cv2.IMREAD_GRAYSCALE)
+    #detect circle centers
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    ret, corners = cv2.findCirclesGrid(gray, grid_size,None,flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
+    if ret:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+        im_with_keypoints = cv2.drawChessboardCorners(gray, (4,11), corners2, ret)
+        found += 1
+        cv2.imshow("img", im_with_keypoints) # display
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+thr_error=0.05
+errors=get_reprojection_errors(objpoints,imgpoints,rvecs,tvecs,mtx,dist)
+valid_args=np.argwhere(errors<thr_error)
+#recalibrate with the selected images
+objpoints_selected=[pt for i,pt in enumerate(objpoints) if i in valid_args]
+imgpts_selected=[pt for i,pt in enumerate(imgpoints) if i in valid_args]
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints_selected, imgpts_selected, gray.shape[::-1], None, None)
+errors=get_reprojection_errors(objpoints_selected,imgpts_selected,rvecs,tvecs,mtx,dist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
